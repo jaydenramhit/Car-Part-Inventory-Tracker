dde39b3257171f701ee7166dbc93bce6042ff6c8
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2022-04-29 12:07:16 -0400

    added more tests for registrationController and fixed css bug

--- tests/registrationController.test.js
+++ tests/registrationController.test.js
@@ -5,7 +5,7 @@ const dbName = "car_testDb";
 const model = require("../models/userModel.js");
 
 var connection;
-beforeEach(async () => { connection = await model.initialize(dbName, true);});
+beforeEach(async () => { connection = await model.initializeUserModel(dbName, true);});
 afterEach(async () => {
     if (connection)
         await connection.end();
@@ -20,7 +20,53 @@ const userData = [
     { username: 'username6', password: 'H3||oW0rld'},
 ]
 
-test("GET /users success case", async () => {
-    let testResponse = await testRequest.get('/');
+test("GET /users fail case due to no users", async () => {
+    let testResponse = await testRequest.get('/users');
     expect(testResponse.status).toBe(404);
 }) 
+
+test("POST /users/signup success case", async () => {
+    let randomUser = userData.at(Math.random() * 6);
+    let testResponse = await testRequest.post('/users/signup').send(randomUser);
+    expect(testResponse.status).toBe(201);
+    
+    let result = await connection.execute("select username from Users;")
+    expect(result[0].length).toBe(1);
+    expect(result[0][0].username).toEqual(randomUser.username);
+})
+
+test("POST /users/signup fail case due to short username", async () => {
+    let badUser = {username: "admin", password: "dANNjdnsu8*&*&832" };
+    let testResponse = await testRequest.post('/users/signup').send(badUser);
+    expect(testResponse.status).toBe(404);
+    
+    let result = await connection.execute("select username from Users;")
+    expect(result[0].length).toBe(0);
+})
+
+test("POST /users/signup fail case due to long username", async () => {
+    let badUser = {username: "THISISAREALLLLLYLONGUSERNAME", password: "dANNjdnsu8*&*&832" };
+    let testResponse = await testRequest.post('/users/signup').send(badUser);
+    expect(testResponse.status).toBe(404);
+    
+    let result = await connection.execute("select username from Users;")
+    expect(result[0].length).toBe(0);
+})
+
+test("POST /users/signup fail case due to weak password", async () => {
+    let badUser = {username: "GoodUsername", password: "password" };
+    let testResponse = await testRequest.post('/users/signup').send(badUser);
+    expect(testResponse.status).toBe(404);
+    
+    let result = await connection.execute("select username from Users;")
+    expect(result[0].length).toBe(0);
+})
+
+test("POST /users/signup fail case due to dropped table", async () => {
+    await connection.execute("DROP TABLE Users;");
+    let randomUser = userData.at(Math.random() * 6);
+
+    let testResponse = await testRequest.post('/users/signup').send(randomUser);
+    expect(testResponse.status).toBe(500);
+
+})
