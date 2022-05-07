cc281443e61a10629a47ceaf479a06d5502d36d7
Braeden Giasson <2043670@johnabbottcollege.net>
2022-05-07 17:23:48 -0400

    Added logger info to validateUtils

--- validateUtils.js
+++ validateUtils.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const validator = require("./node_modules/validator/validator.js");
+const logger = require('./logger');
 
 /**
  * Validates that the specified car part name is an alphanumeric string.
@@ -8,12 +9,14 @@ const validator = require("./node_modules/validator/validator.js");
  * @returns True if the name is an alphanumeric string; otherwise false.
  */
 function isValid(name){
-    return validator.isAlphanumeric(name,  ['en-US'], {'ignore': ' _-'});
-    // if (validator.isAlphanumeric(name,  ['en-US'], {'ignore': ' _-'}))
-    //     return true;
-    // else
-    //     return false;
-
+    if (validator.isAlphanumeric(name,  ['en-US'], {'ignore': ' _-'})){
+        logger.info(`IS an alphanumeric string ${name} - car part name -- isValid`);
+        return true;
+    }
+    else{
+        logger.info(`NOT an alphanumeric string ${name} - car part name  -- isValid`);
+        return false;
+    }
 }
 
 /**
@@ -22,21 +25,25 @@ function isValid(name){
  * @returns True if the part number is numeric; otherwise false.
  */
 function isPartNumber(partNumber){ 
-    return validator.isNumeric(partNumber.toString());
-    // if (validator.isNumeric(partNumber.toString()))
-    //     return true;
-    // else
-    //     return false;
+    if (validator.isNumeric(partNumber.toString())){
+        logger.info(`IS an numeric ${partNumber} - car part name  -- isPartNumber`);
+        return true;
+    }
+    else{
+        logger.info(`NOT an numeric ${partNumber} - car part name  -- isPartNumber`);
+        return false;
+    }
 }
 
 /**
  * Validates that the specified string is a valid URL.
- * // https://thispointer.com/javascript-check-if-string-is-url/
+ * https://thispointer.com/javascript-check-if-string-is-url/
  * @param {*} string The specified string to validate.
  * @returns True if the specified string is a valid URL; otherwise false.
  */
 function isURL(string) {
     const matchpattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
+    logger.info(`Currently validating url ${url} -- isPartNumber`);
     return matchpattern.test(string);
 }
 
@@ -46,11 +53,14 @@ function isURL(string) {
  * @returns True if the specified string is empty; otherwise false.
  */
 function stringIsEmpty(string){
-    return string === "";
-    // if (string == "")
-    //     return true;
-    // else
-    //     return false;
+    if (string == ""){
+        logger.info(`IS empty string ${string} - checks if string is empty-- stringIsEmpty`);
+        return true;
+    }
+    else{
+        logger.info(`NOT an empty string ${string} - checks if string is empty-- stringIsEmpty`);
+        return false;
+    }
 }
 
 
